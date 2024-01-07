e8bfd1e5b63da248abc726cdcdbb86db9af3e79f
KeelyRyan <keelyannryan@gmail.com>
2024-01-07 14:11:41 +0000

    Build Search fo Flight Deals

--- KeelyFlightDeals/flight_search.py
+++ KeelyFlightDeals/flight_search.py
@@ -1,8 +1,10 @@
 import requests
 import config
+from flight_data import FlightData
 
 TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com/locations/query"
 TEQUILA_API_KEY = config.TEQUILA_API_KEY
+TEQUILA_SEARCH_API ="https://api.tequila.kiwi.com/v2/search"
 
 class FlightSearch:
     def __init__(self):
@@ -23,3 +25,47 @@ class FlightSearch:
         self.iata = result['locations'][0]['code']
 
         return self.iata
+
+    def get_flight_data(self, from_iata, destination_iata, from_time, to_time ):
+        headers = {
+            "apikey": TEQUILA_API_KEY
+        }
+        params = {
+            "fly_from": from_iata,
+            "fly_to": destination_iata,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "one_for_city": 1,
+            "max_stopovers": 1,
+            "curr": "EUR"
+
+        }
+        response = requests.get(url=f"{TEQUILA_SEARCH_API}",
+                                headers=headers,
+                                params=params)
+
+        try:
+            data = response.json()['data'][0]
+
+        except IndexError:
+            print(f"No flights found for {from_iata} to {destination_iata}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0],
+            availability=data["availability"]["seats"]
+        )
+        print(f"{flight_data.destination_city}: "
+              f"€{flight_data.price} "
+              f"Date:{flight_data.out_date} "
+              f"Seats:{flight_data.availability}")
+
+        return data
--- KeelyFlightDeals/main.py
+++ KeelyFlightDeals/main.py
@@ -1,14 +1,16 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 from flight_search import FlightSearch
+from datetime import datetime as dt, timedelta
 
-import pprint
+tomorrow = dt.now() + timedelta(days=1)
+six_month_from_today = dt.now() + timedelta(days=(6 * 30))
 
 data = DataManager()
 sheet_data = data.get_data()
-#print(sheet_data)
-
 flight_search = FlightSearch()
+
+ORIGIN_IATA = "DUB"
 
 if sheet_data[0]['iataCode'] == '':
     for flight in sheet_data:
@@ -16,6 +18,18 @@ if sheet_data[0]['iataCode'] == '':
 
     print(sheet_data)
 
-data.prices = sheet_data
-data.update_iata()
+# data.prices = sheet_data
+# data.update_iata()
 
+for flight in sheet_data:
+    flight_data = flight_search.get_flight_data(
+            from_iata=ORIGIN_IATA,
+            destination_iata=flight["iataCode"],
+            from_time=tomorrow,
+            to_time=six_month_from_today)
+    #print(flight_data)
+
+
+    #print(flight_search.get_flight_data(flight['iataCode']))
+
+#print(flight_data)
